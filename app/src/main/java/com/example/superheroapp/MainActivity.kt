2fbe80110db8fa8c16006d487905e9c6a5f8a524
renzo <awekendo@icloud.com>
2024-07-23 00:08:59 -0500

    first commit

---
+++ app/src/main/java/com/example/superheroapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.superheroapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.superheroapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUi()
+        }
+
+    private fun initUi() {
+        binding.Searchview.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchByName(query.orEmpty())
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?)=false
+
+        })
+    }
+
+    private fun searchByName(query: String) {
+
+    }
+}
